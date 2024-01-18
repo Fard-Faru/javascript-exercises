b130a66f6c3efb5283375cef6d335dd2247fc501
Fardin <73206587+Fard-Faru@users.noreply.github.com>
2024-01-17 22:27:29 -0500

    etch

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let OldAge = -1;
+    let Oldperson;
+    arr.forEach(person => {
+        if((person.yearOfDeath - person.yearOfBirth) > OldAge){
+            Oldperson = person;
+            OldAge = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    return Oldperson;
 };
 
 // Do not edit below this line
